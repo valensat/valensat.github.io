190b09ba65b8f6c832fe67e4718bb2ae81cfe1e0
valensat <valensatproject@gmail.com>
2023-03-31 12:52:09 +0200

    actualizaciones

--- js/app.js
+++ js/app.js
@@ -42,9 +42,55 @@
     }
 
     menu.addEventListener('click', ()=> list.classList.toggle('menu__links--show'));
+})();
 
+(function(){
+    const listElements = document.querySelectorAll('.menu__item--show--index');
+    const list = document.querySelector('.menu__links--index');
+    const menu = document.querySelector('.menu__hamburguer');
 
-    
+    const addClick = ()=>{
+        listElements.forEach(element =>{
+            element.addEventListener('click', ()=>{
+                
+                let subMenu = element.children[1];
+                let height = 0;
+                element.classList.toggle('menu__item--active--index');
+
+                if(subMenu.clientHeight === 0){
+                    height = subMenu.scrollHeight;
+                }
+
+                subMenu.style.height = `${height}px`
+            })
+        })
+    }
+
+    const deleteStyleHeight = ()=>{
+        listElements.forEach(element=>{
+
+            if(element.children[1].getAttribute('style')){
+                element.children[1].removeAttribute('menu__item--index--active');
+            }
+        });
+    }
+
+    window.addEventListener('resize', ()=>{
+        if(window.innerWidth > 1500){
+            deleteStyleHeight();
+        }else{
+            addClick();
+        }
+    });
+
+    if(window.innerWidth <= 1500){
+        addClick();
+    }
+
+    menu.addEventListener('click', ()=> list.classList.toggle('menu__links--show--index'));
+})();
+
+(function(){
     // BOTÓN VOLVER ARRIBA
 
     var backToTop = document.querySelector(".back-to-top");
@@ -63,6 +109,5 @@
         behavior: "smooth"
     });
     });
-
 })();
 
