9ddc649c166cae36047347186e1fae9f428807cf
valensat <valensatproject@gmail.com>
2024-01-10 15:17:41 +0100

    nuevo home

--- js/slider.js
+++ js/slider.js
@@ -3,6 +3,7 @@
     const sliders = [...document.querySelectorAll('.testimony_body')];
     const buttonNext = document.querySelector('#next');
     const buttonBefore = document.querySelector('#before');
+    const testimonies = [...document.querySelectorAll('.testimony')];
     let value;
 
     buttonNext.addEventListener('click', ()=>{
@@ -24,6 +25,23 @@
         }
 
         sliders[value-1].classList.add('testimony_body--show')
+        document.querySelector('.testimony').setAttribute('data-slide', value);
     }
 
 })();
+
+document.addEventListener('DOMContentLoaded', function() {
+    var productosLink = document.getElementById('productosLink');
+    var productosMenu = document.getElementById('productosMenu');
+
+    productosLink.addEventListener('click', function() {
+        productosMenu.classList.toggle('active');
+    });
+
+    // Cierra el menú cuando haces clic fuera de él
+    document.addEventListener('click', function(event) {
+        if (!productosMenu.contains(event.target) && !productosLink.contains(event.target)) {
+            productosMenu.classList.remove('active');
+        }
+    });
+});
