1895d00a23f60c4d569f79cc1960a3b1ff71d1dc
valensat <valensatproject@gmail.com>
2024-01-19 14:01:25 +0100

    cosas

---
+++ js/products_page.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Function to show a section
+    window.showSection = function(sectionId) {
+        // Remove 'active' class from all menu items
+        document.querySelectorAll('.menu-item_product_page').forEach(function(menuItem) {
+            menuItem.classList.remove('active');
+        });
+
+        // Hide all sections
+        document.querySelectorAll('.content-section_product_page').forEach(function(section) {
+            section.style.opacity = '0';
+            section.style.visibility = 'hidden';
+        });
+    
+        // Use a timeout to allow the hide transition to play
+        setTimeout(function() {
+            // Show the selected section
+            var selectedSection = document.getElementById(sectionId);
+            selectedSection.style.opacity = '1';
+            selectedSection.style.visibility = 'visible';
+            document.querySelector('.menu-item_product_page[onclick="showSection(\'' + sectionId + '\')"]').classList.add('active');
+        }, 300); // Should match the duration of the CSS transition
+    };
+  
+    // Show 'ciencia' section by default on page load
+    showSection('ciencia_product_page');
+});
